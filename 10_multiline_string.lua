e04a4fdef2851e78699c6a502c6709a26ed14329
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-10 07:52:58 +0100

    Added comments

--- 10_multiline_string.lua
+++ 10_multiline_string.lua
@@ -1,2 +1,6 @@
+--
+-- Skript transpilovaný do jazyka Lua
+--
+
 local message = "Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit, sed do\neiusmod tempor incididunt ut labore et\ndolore magna aliqua. Ut enim ad minim\nveniam, quis nostrud exercitation ullamco\nlaboris nisi ut aliquip ex ea commodo\nconsequat."
 return print(message)
