d6be445c3a247a5ddf09562ec970fce4595482f2
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-10 07:50:45 +0100

    Script 46_slicing.moon transpiled into Lua

---
+++ 46_slicing.lua
@@ -0,0 +1,39 @@
+--
+-- Skript transpilovaný do jazyka Lua
+--
+
+local print_list
+print_list = function(list)
+  print("i", "item")
+  for i, item in ipairs(list) do
+    print(i, item)
+  end
+  return print()
+end
+local list = {
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10
+}
+local slice
+do
+  local _accum_0 = { }
+  local _len_0 = 1
+  local _max_0 = 8
+  for _index_0 = 4, _max_0 < 0 and #list + _max_0 or _max_0 do
+    local item = list[_index_0]
+    if item % 2 == 0 then
+      _accum_0[_len_0] = item
+      _len_0 = _len_0 + 1
+    end
+  end
+  slice = _accum_0
+end
+return print_list(slice)
