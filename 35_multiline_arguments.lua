c6a5cfdef1bcbb1e8de53e13eda4cf9059c580b8
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-10 07:50:45 +0100

    Script 35_multiline_arguments.moon transpiled into Lua

---
+++ 35_multiline_arguments.lua
@@ -0,0 +1,9 @@
+--
+-- Skript transpilovaný do jazyka Lua
+--
+
+local supersum
+supersum = function(a, b, c, d, e, f, g, h, i, j)
+  return a + b + c + d + e + f + g + h + i + j
+end
+return print(supersum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
