8ef750db379ba5ea21eea27091ac352dd6b5fbb7
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-10 07:50:45 +0100

    Script 39_list_comprehension.moon transpiled into Lua

---
+++ 39_list_comprehension.lua
@@ -0,0 +1,32 @@
+--
+-- Skript transpilovaný do jazyka Lua
+--
+
+local print_list
+print_list = function(list)
+  print("i", "item")
+  for i, item in ipairs(list) do
+    print(i, item)
+  end
+  return print()
+end
+local list = {
+  1,
+  2,
+  3,
+  4,
+  5
+}
+print_list(list)
+local doubled
+do
+  local _accum_0 = { }
+  local _len_0 = 1
+  for _index_0 = 1, #list do
+    local item = list[_index_0]
+    _accum_0[_len_0] = item * 2
+    _len_0 = _len_0 + 1
+  end
+  doubled = _accum_0
+end
+return print_list(doubled)
