5927a795f1be914c7d3e194aada59bb921a53d09
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-15 07:37:20 +0100

    Example #48: for-loop and slicing

---
+++ 48_for_slicing.lua
@@ -0,0 +1,21 @@
+--
+-- Skript transpilovaný do jazyka Lua
+--
+
+local items = {
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10
+}
+local _max_0 = 8
+for _index_0 = 1, _max_0 < 0 and #items + _max_0 or _max_0 do
+  local item = items[_index_0]
+  print(item)
+end
